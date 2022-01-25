8aa2081e6beab915fbd07b1e45f774753e625a5d
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-25 12:12:43 -0800

    fixed some filing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-// import { getCharacters } from './services/characters';
-// import { useEffect } from 'react';
-// import './App.css';
+import { getCharacters } from './services/characters';
+import { useEffect } from 'react';
+import './App.css';
 
-// function App() {
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const charactersData = await getCharacters();
-//       console.log(charactersData);
-//     };
-//     fetchData();
-//   });
+function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const charactersData = await getCharacters();
+      console.log(charactersData);
+    };
+    fetchData();
+  });
 
-//   return <div className="App"></div>;
-// }
+  return <div className="App"></div>;
+}
 
-// export default App;
+export default App;
