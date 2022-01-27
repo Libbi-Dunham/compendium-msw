254256f779d78a3922ff42f7f039eb3c6bda5ef1
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-27 15:08:01 -0800

    got app test to pass with msw

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { user } from './data/data';
 import userEvent from '@testing-library/user-event';
 import App from './App';
+
+const server = setupServer(
+  rest.get('https://rickandmortyapi.com/api/character/rest/v1/users', (req, res, ctx) => {
+    const select = req.url.searchParams.get('select');
+    if (select === '*') {
+      return res(ctx.json([user]));
+    }
+    return res(ctx.status(500));
+  })
+);
+
+beforeAll(() => server.listen());
+
+afterAll(() => server.close());
 
 test('renders the search bar', async () => {
   render(<App />);
