e9b8f3ba6a4fda2f753854d6fae8b5c8f6013f3c
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-28 15:23:35 -0800

    fixed app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { user } from './data/data';
+import { characters } from './data/data';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
 const server = setupServer(
   rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
-    return res(ctx.json({ results: user }));
+    return res(ctx.json({ results: characters }));
   })
 );
 
@@ -18,10 +18,11 @@ afterAll(() => server.close());
 test('renders the search bar', async () => {
   render(<App />);
   const searchBar = await screen.findByRole('textbox');
-
   const characterName = 'Rick';
 
   userEvent.type(searchBar, characterName);
+  const characterElement = await screen.findByAltText(/Rick Sanchez/i);
 
   expect(searchBar).toBeInTheDocument();
+  expect(characterElement).toBeInTheDocument();
 });
